be17588559c45faafd5fa262874db861793eed43
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-04-11 14:08:08 -0600

    Introduction implemented.

--- Mars.java
+++ Mars.java
@@ -13,7 +13,15 @@ public class Mars {
         System.out.println("...");
         System.out.println("...");
         System.out.println("Game initialized.");
+        //User inputs their name here.
+        System.out.println("Greetings, what is your name?");
+        Scanner input = new Scanner(System.in);
+        String username = input.nextLine();
 
+        //Printing Game introduction
+        System.out.println("Hello "+username+". Welcome to the Mars Adventure Game.");
+        System.out.println("Here's the deal. NASA has spotted movement on Mars, and by movement we mean alien forces mobilizing for an invasion of Earth.");
+        System.out.println("You've been selected to go to Mars for reconoscence and perhaps sabotage. (Lucky you.)");
 
     }
 }
