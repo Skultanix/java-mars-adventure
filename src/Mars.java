f1915d30729dc7429ca674df6e31847e3c6a22fa
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-04-11 14:50:45 -0600

    Moved .java file to better directory. Implemented User mood feedback response.

---
+++ src/Mars.java
@@ -0,0 +1,48 @@
+//Importing the scanner utility
+import java.util.Scanner;
+//Iporting InputMismatchException utility to regulate input errors
+import java.util.InputMismatchException;
+
+
+//Set up of the class "Mars
+public class Mars {
+    public static void main(String[] args) {
+        //Game initialization
+        System.out.println("Mars Adventure initializing.");
+        System.out.println("...");
+        System.out.println("...");
+        System.out.println("...");
+        System.out.println("Game initialized.");
+        //User inputs their name here.
+        System.out.println("Greetings, what is your name?");
+        Scanner input = new Scanner(System.in);
+        String username = input.nextLine();
+
+        //Printing Game introduction
+        System.out.println("Hello " + username + ". Welcome to the Mars Adventure Game.");
+        System.out.println("Here's the deal. NASA has spotted movement on Mars, and by movement we mean alien forces mobilizing for an invasion of Earth.");
+        System.out.println("You've been selected to go to Mars for recon and perhaps sabotage. (Lucky you.)");
+
+        //Ask for User response to mission.
+        System.out.println("Do you look forward to your mission? (y or n)");
+
+        //Responsive feedback to user response
+        while (true) {
+            //Declare variable for response
+            String missionMood = input.nextLine();
+
+            if (missionMood.equals("y")) {
+                System.out.println("Your ancestors smile on your courage. A fate in Valhalla seems likely");
+                break;
+            } else if (missionMood.equals("n")) {
+                System.out.println("Your wariness of the dangerous unknown is prudent, albeit uninspiring.");
+                break;
+            } else {
+                System.out.println("Invalid response please try again.");
+            }
+        }
+
+
+    }
+}
+
