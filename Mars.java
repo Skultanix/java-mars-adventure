ce5042bdf2505d6443c7a0e9ca1b29f9245e2694
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-04-11 14:04:16 -0600

    Initial commit.

---
+++ Mars.java
@@ -0,0 +1,19 @@
+//Importing the scanner utility
+import java.util.Scanner;
+//Iporting InputMismatchException utility to regulate input errors
+import java.util.InputMismatchException;
+
+
+//Set up of the class "Mars
+public class Mars {
+    public static void main(String[] args)  {
+        //Game initialization
+        System.out.println("Mars Adventure initializing.");
+        System.out.println("...");
+        System.out.println("...");
+        System.out.println("...");
+        System.out.println("Game initialized.");
+
+
+    }
+}
